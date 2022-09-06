3e88390891c78f0269f579250c877953d4b733a5
Heverton Lourenço <heverton30stm@gmail.com>
2022-09-06 15:21:54 -0300

    Q6 lista - pilhas

---
+++ Lista-6/Q6.py
@@ -0,0 +1,16 @@
+# 6. Crie uma função que copia todos os elementos de pilha p para outra pilha q, de forma que os elementos mantenham a
+# mesma ordem, ou seja, o elemento no topo da pilha p permanecerá no topo da pilha q
+
+def stackCopy(pilha, copia=[]):
+    if len(pilha) == 0:
+        return pilha
+    temp = pilha.pop()
+    stackCopy(pilha)
+    copia.append(temp)
+    pilha.append(temp)
+    return copia
+
+l = [1,2,3,4,5,6]
+print(stackCopy(l))
+print(l)
+    
